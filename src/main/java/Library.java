c2cf159d0b37411fa10db98598797a345030949d
khleungat <khleungat@csl4wk43.win2k.cs.ust.hk>
2019-02-21 10:38:46 +0800

    Modify Library.java

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -3,6 +3,8 @@
  */
 public class Library {
 	public static void main(String[] args){
+		Library lib = new Library();
+		if(lib.someLibraryMethod())
 		System.out.println("When there is a fire, commit and push.");
 	}
     public boolean someLibraryMethod() {
